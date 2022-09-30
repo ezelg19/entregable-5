f6c4b44ae93baa3c0ae6fa95f723c0fee39255fa
ezelg19 <51120861+ezelg19@users.noreply.github.com>
2022-09-30 00:35:42 -0300

    Add files via upload

--- pug/server.js
+++ pug/server.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const router = require('./router.js')
+const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.set('views','./views');
+app.set('view engine', 'pug');
+
+app.get('/',(req,res)=>{
+    res.render('index.pug')
+})
+
+app.use('/productos', router)
+
+
+const PORT = 4000
+app.listen(PORT,()=>console.log(`server en el puerto ${PORT}`))
